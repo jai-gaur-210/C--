44bd50d462d23c913a28d921e47cc5779f0ae192
jai <jaigaur.imp210@gmail.com>
2023-03-15 18:36:29 +0530

    commit

---
+++ DataType/DataType.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a; //declaration
+    a=12; //intialisation
+
+    cout<<"size of int "<<sizeof(a)<<endl;
+
+    float b;
+    cout<<"size of int "<<sizeof(b)<<endl;
+
+    char c;
+    cout<<"size of int "<<sizeof(c)<<endl;
+    
+    bool d;
+    cout<<"size of int "<<sizeof(d)<<endl;
+
+    short int si;
+    long int li;
+    cout<<"size of shortint "<<sizeof(si)<<endl;
+    cout<<"size of longint "<<sizeof(li)<<endl;
+    
+
+
+return 0;    
+
+}
