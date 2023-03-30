249e396e7a9c6f52f4ae4d7824997ff6627f7afe
jai <jaigaur.imp210@gmail.com>
2023-03-30 17:56:48 +0530

    c

---
+++ Functions/PythogorianTriplet.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool check(int x, int y, int z){
+
+    int a= max(x, max(y,z));
+    int b, c;
+    
+    if(a==x){
+
+        b=y;
+        c=z;
+    }
+    else{
+        b=x;
+        c=y;
+    }
+
+    if(a*a==b*b + c*c){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+int32_t main() {
+    int x,y,z;
+    cin>>x>>y>>z;
+
+    if(check(x,y,z)){
+
+        cout<<"Pythogorian triplet";
+    }
+    else{
+        cout<<"Not a pythogorian triplet";
+    }
+
+    return 0;
+}
