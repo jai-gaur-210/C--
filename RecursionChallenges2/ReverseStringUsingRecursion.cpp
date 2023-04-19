e165dcd4afff250234f1262ee98357a46583d96b
jai <jaigaur.imp210@gmail.com>
2023-04-19 17:57:37 +0530

    c

---
+++ RecursionChallenges2/ReverseStringUsingRecursion.cpp
@@ -0,0 +1,24 @@
+// "binod"     print b
+// "inod"      print i
+// "nod"       print n
+// "od"        print o
+// "d"         print d
+// ""          return
+#include<iostream>
+using namespace std;
+
+void reverse(string s){
+    
+    if(s.length()==0){ // base case
+        return ;
+    }
+
+    string ros = s.substr(1);
+    reverse(ros);
+    cout<<s[0];
+}
+
+int main(){
+    reverse("binod");
+    return 0;
+}
